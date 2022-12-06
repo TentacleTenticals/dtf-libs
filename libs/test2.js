93dc83b1a86e2a24dd252e2f880ac7046c81426b
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-12-06 13:08:30 +0300

    Create test2.js

---
+++ libs/test2.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Test
+// @namespace   https://github.com/TentacleTenticals
+// @match       https://dtf.ru/*
+// @grant       none
+// @version     1.1
+// @author      Tentacle Tenticals
+// @description Test
+// @homepage https://github.com/TentacleTenticals/
+// @license MIT
+// ==/UserScript==
+/* jshint esversion:8 */
+
+(function() {
+'use strict';
+  let ha = 'Yo!';
+})();
