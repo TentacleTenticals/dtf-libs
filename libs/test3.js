79d5dcbb47145cc215643c047c6e7d3a013d3b98
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-12-06 13:49:25 +0300

    Create test3.js

---
+++ libs/test3.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        DTF observer
+// @namespace   https://github.com/TentacleTenticals/dtf-libs
+// @match       https://dtf.ru/*
+// @grant       none
+// @version     1.0
+// @author      Tentacle Tenticals
+// @description Обсервер DTF
+// @homepage https://github.com/TentacleTenticals/dtf-libs
+// @license MIT
+// ==/UserScript==
+/* jshint esversion:8 */
+
+let test3 = 'Baka!';
