93bd9c5b5d299b4d475022145a87d30159e35e6f
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-01-05 19:33:52 +0300

    Update main.js

--- libs/settings/main.js
+++ libs/settings/main.js
@@ -11,6 +11,81 @@
 // ==/UserScript==
 /* jshint esversion:8 */
 
+let style = `
+.DTF-settingsOpener {
+  top: 19px;
+  right: 257px;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  flex-wrap: nowrap;
+  z-index: 1000;
+  color: rgb(0 0 0);
+  padding: 2px 2px 2px 2px;
+  line-height: unset;
+  font-size: 19px;
+  background: rgb(255 255 255);
+  box-shadow: 0px 0px 2px 1px rgb(0 0 0);
+}
+.DTF-settingsOpener .label {
+  display: none;
+  position: absolute;
+  float: left;
+  font-size: 12px;
+  text-align: center;
+  top: 5px;
+  left: 5px;
+}
+.DTF-settingsOpener:hover .label {
+  display: block;
+}
+.DTF-settingsOpener .list {
+  display: none;
+  background: rgb(255,255,255);
+  color: rbg(0,0,0);
+}
+.DTF-settingsOpener:hover .list,
+.DTF-settingsOpener .list:hover {
+  display: block;
+  background: rgb(255,255,255);
+  margin-top: 3px;
+  padding: 1px 3px 3px 3px;
+  box-shadow: 0px 0px 2px 1px rgb(0 0 0);
+}
+.DTF-settingsOpener .list .btn {
+  background: rgb(216 234 249);
+  font-size: 13px;
+  border: 1px solid rgb(0 0 0);
+  border-radius: 3px;
+  padding: 1px 3px 1px 3px;
+  cursor: pointer;
+}
+.DTF-settingsOpener .list .btn:hover {
+  background: rgb(203 232 255);
+}
+
+.DTF-settings {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  z-index: 115;
+  background: rgb(255 255 255);
+  padding: 3px;
+  max-width: 50%;
+  box-shadow: 0px 0px 2px 1px rgb(0 0 0);
+}
+.DTF-settings input {
+  width: max-content;
+}
+.DTF-settings fieldset {
+  border: 1px solid black;
+  margin: 5px 0px 5px 0px;
+  padding: 3px;
+}`;
+
+new CSS(false, style);
+
 // Активация при загрузке страницы
 function onPageLoad(run){
   {
