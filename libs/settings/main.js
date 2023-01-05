d07a16fe18bc8226ee63e79b2fa948c1ade32396
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-01-05 16:42:39 +0300

    Update main.js

--- libs/settings/main.js
+++ libs/settings/main.js
@@ -11,6 +11,24 @@
 // ==/UserScript==
 /* jshint esversion:8 */
 
+// Активация при загрузке страницы
+function onPageLoad(run){
+  {
+  const log = console.log.bind(console)
+  console.log = (...args) => {
+    if(Array.isArray(args)){
+      if(args[0]){
+        if(typeof args[0] === 'string'){
+          if(args[0].match(/\[ Air \] Ready.*/)){
+            run();
+          }
+        }
+      }
+    }
+    log(...args);
+  }}
+};
+
 // Класс для добавления CSS
 class Css{
   constructor(title, style){
